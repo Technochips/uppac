8ff9400e8a1cf772ceba15e61b5ef43cc159ee1c
Technochips <julienfillol2@gmail.com>
2017-12-01 15:25:18 +0100

    configurations n stuff is now in a external file

---
+++ config.py
@@ -0,0 +1,6 @@
+UPDATE_BEFORE = "Upgrading "
+UPDATE_AFTER = "..."
+SKIP_BEFORE = "Skipping "
+SKIP_AFTER = "..."
+SKIP_NOTROOT = "Reason: You need root privileges."
+SKIP_DOESNTEXIST = "Reason: Doesn't exists."
--- uppac.py
+++ uppac.py
@@ -4,33 +4,28 @@ import os
 
 is_root = (os.geteuid() == 0)
 
-UPDATE_BEFORE = "Upgrading "
-UPDATE_AFTER = "..."
-SKIP_BEFORE = "Skipping "
-SKIP_AFTER = "..."
-SKIP_NOTROOT = "Reason: You need root privileges."
-SKIP_DOESNTEXIST = "Reason: Doesn't exists."
+import config
 
 if os.path.exists("/bin/apt") or os.path.exists("/usr/bin/apt"):
     if is_root:
-        print(UPDATE_BEFORE + "apt" + UPDATE_AFTER)
+        print(config.UPDATE_BEFORE + "apt" + config.UPDATE_AFTER)
         os.system("apt update -y")
         os.system("apt upgrade -y")
         os.system("apt auto-remove -y")
     else:
-        print(SKIP_BEFORE + "apt" + SKIP_AFTER)
-        print(SKIP_NOTROOT)
+        print(config.SKIP_BEFORE + "apt" + config.SKIP_AFTER)
+        print(config.SKIP_NOTROOT)
 else:
-    print(SKIP_BEFORE + "apt" + SKIP_AFTER)
-    print(SKIP_DOESNTEXIST)
+    print(config.SKIP_BEFORE + "apt" + config.SKIP_AFTER)
+    print(config.SKIP_DOESNTEXIST)
 
 if os.path.exists("/bin/pacman") or os.path.exists("/usr/bin/pacman"):
     if is_root:
-        print(UPDATE_BEFORE + "pacman" + UPDATE_AFTER)
+        print(config.UPDATE_BEFORE + "pacman" + config.UPDATE_AFTER)
         os.system("pacman -Syu")
     else:
-        print(SKIP_BEFORE + "pacman" + SKIP_AFTER)
-        print(SKIP_NOTROOT)
+        print(config.SKIP_BEFORE + "pacman" + config.SKIP_AFTER)
+        print(config.SKIP_NOTROOT)
 else:
-    print(SKIP_BEFORE + "pacman" + SKIP_AFTER)
-    print(SKIP_DOESNTEXIST)
+    print(config.SKIP_BEFORE + "pacman" + config.SKIP_AFTER)
+    print(config.SKIP_DOESNTEXIST)
